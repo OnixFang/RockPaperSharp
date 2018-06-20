fef4953ff77d373bf4ff3d33cb0b2c6c9af737c6
OnixFang <twily.dev@outlook.com>
2018-06-19 22:00:43 -0400

    Finish game

--- RockPaperSharp/Program.cs
+++ RockPaperSharp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RockPaperSharp
 {
@@ -22,11 +18,101 @@ namespace RockPaperSharp
         public static void StartGame()
         {
             GameScreen.DrawPlayerChoices();
+            Computer.GenerateChoice();
+            GameScreen.DrawResults(CompareChoices());
+            PlayAgain();
         }
 
-        static void CompareChoices()
+        static void PlayAgain()
         {
+            int decision;
 
+            Console.WriteLine("Play again? 1. Yes, 2. No");
+
+            try
+            {
+                decision = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                decision = 0;
+            }
+
+            if (decision == 1)
+            {
+                StartGame();
+            }
+        }
+
+        static string CompareChoices()
+        {
+            string result;
+
+            switch (Player.Choice)
+            {
+                case Choices.Rock:
+                    switch (Computer.Choice)
+                    {
+                        case Choices.Rock:
+                            result = "Draw!";
+                            break;
+                        case Choices.Paper:
+                            result = "Computer wins!!";
+                            ComputerScore++;
+                            break;
+                        case Choices.Scisors:
+                            result = "Player wins!";
+                            PlayerScore++;
+                            break;
+                        default:
+                            result = "An error occurred.";
+                            break;
+                    }
+                    break;
+                case Choices.Paper:
+                    switch (Computer.Choice)
+                    {
+                        case Choices.Rock:
+                            result = "Player wins!";
+                            PlayerScore++;
+                            break;
+                        case Choices.Paper:
+                            result = "Draw!";
+                            break;
+                        case Choices.Scisors:
+                            result = "Computer wins!!";
+                            ComputerScore++;
+                            break;
+                        default:
+                            result = "An error occurred.";
+                            break;
+                    }
+                    break;
+                case Choices.Scisors:
+                    switch (Computer.Choice)
+                    {
+                        case Choices.Rock:
+                            result = "Computer wins!!";
+                            ComputerScore++;
+                            break;
+                        case Choices.Paper:
+                            result = "Player wins!";
+                            PlayerScore++;
+                            break;
+                        case Choices.Scisors:
+                            result = "Draw!";
+                            break;
+                        default:
+                            result = "An error occurred.";
+                            break;
+                    }
+                    break;
+                default:
+                    result = "An error occurred.";
+                    break;
+            }
+
+            return result;
         }
 
         static class GameScreen
@@ -34,16 +120,21 @@ namespace RockPaperSharp
             static void DrawTitle()
             {
                 Console.WriteLine("------------------------------- RockPaperSharp -------------------------------");
+                Console.WriteLine();
             }
 
             static void DrawScores()
             {
                 Console.WriteLine("Player Wins  : " + PlayerScore);
                 Console.WriteLine("Computer Wins: " + ComputerScore);
+                Console.WriteLine();
             }
 
             internal static void DrawPlayerChoices()
             {
+                int UserInput;
+
+                Console.Clear();
                 DrawTitle();
                 DrawScores();
 
@@ -53,29 +144,59 @@ namespace RockPaperSharp
                 Console.WriteLine("3. Scisors" + Environment.NewLine);
                 Console.Write("Enter the number of your weapon: ");
 
-                Player.Choice = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    UserInput = Convert.ToInt32(Console.ReadLine());
+                    if (UserInput > 3)
+                    {
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        Player.Choice = (Choices)UserInput;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("This is not a valid answer...");
+                    Console.Write("Press enter to continue");
+                    Console.ReadKey();
+                    DrawPlayerChoices();
+                }
+            }
+
+            internal static void DrawResults(string result)
+            {
+                Console.Clear();
+                DrawTitle();
+                DrawScores();
+
+                Console.WriteLine($"Player's choice: {Player.Choice}");
+                Console.WriteLine($"Computer's choice: {Computer.Choice}");
+
+                Console.WriteLine();
+                Console.WriteLine(result);
+                Console.WriteLine();
             }
         }
 
         static class Player
         {
-            internal static int Choice { get; set; }
+            internal static Choices Choice { get; set; }
         }
 
         static class Computer
         {
-            internal static int Choice = 1;
+            internal static Choices Choice = (Choices)1;
 
             internal static void GenerateChoice()
             {
                 Random rnd = new Random();
 
-                Choice = rnd.Next(1, 4);
+                Choice = (Choices)rnd.Next(1, 4);
             }
         }
     }
-
-
 
     enum Choices
     {
